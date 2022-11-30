9b5f3d56b566289230e880b61e280a6156bae0cc
Kayode Segun <kay_1oneway@yahoo.com>
2022-11-29 20:53:45 -0800

    Tasks on if-elif statements completed

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Kayode Oluwasegun
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Kayode Oluwasegun
+
+def pow(a, b):
+    return (a ** b)
